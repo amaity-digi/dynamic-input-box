8b5e60f7ce0850bdddccd53ea679f5cf60223fe4
amaity-digi <116334087+amaity-digi@users.noreply.github.com>
2023-09-10 17:22:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+ function App() {
+  const [box, setBox] = useState([
+    {
+      index: 0,
+      value: ""
+    }
+  ]);
+
+  const handleClick = () => {
+    setBox([
+      ...box,
+      {
+        index: box.length,
+        value: ""
+      }
+    ]);
+  };
+
+  const handleChange = (e, index) => {
+    const insertValue = [...box];
+    const updatedIndex = insertValue.findIndex((val) => val.index === index);
+    insertValue[updatedIndex]["value"] = e.target.value;
+    setBox(insertValue);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {box.map(({ index }) => (
+        <div key={index} style={{ margin: "5px" }}>
+          <input
+            value={box[index]["value"]}
+            onChange={(e) => handleChange(e, index)}
+          />
+        </div>
+      ))}
+      <button onClick={handleClick}>Add New Input</button>
+      <div>
+        {box.length > 1 &&
+          box.map(({ index, value }) => (
+            <div key={index}>
+              Value {index + 1}: {value}
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
 
 export default App;
+
